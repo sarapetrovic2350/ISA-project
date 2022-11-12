45787a3c62ad555b20ac1ace2f6c9d0bfc643f2e
Sara Petrovic <petrovic.ra21.2019@uns.ac.rs>
2022-11-12 15:35:27 +0100

    Added toolbar and modules to frontend project

--- BloodBankFrontend/src/app/app-routing.module.ts
+++ BloodBankFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserRegistrationComponent } from './components/user-registration/user-registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user-registration',
+    component: UserRegistrationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- BloodBankFrontend/src/app/app.module.ts
+++ BloodBankFrontend/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { UserRegistrationComponent } from './components/user-registration/user-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
