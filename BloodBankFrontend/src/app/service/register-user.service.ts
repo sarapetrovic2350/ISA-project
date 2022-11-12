c30f0f2dd7cf3026a11f05675d7a39bcff7365c4
Sara Petrovic <petrovic.ra21.2019@uns.ac.rs>
2022-11-12 21:16:38 +0100

    Added registration form and implemented basic registration

--- BloodBankFrontend/src/app/service/register-user.service.ts
+++ BloodBankFrontend/src/app/service/register-user.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../model/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RegisterUserService {
 
-  constructor() { }
+export class RegisterUserService {
+  apiHost: string = 'http://localhost:8091/';
+  url = this.apiHost + 'user/registerUser';
+  headers: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  constructor(
+    private http: HttpClient
+    ) {}
+
+    registerUser(user: User) {
+      return this.http.post<any>(this.url, user);
+    }
 }
