75c759f3b8ee290e3e6d7ec2945c71bffa9ef60d
Sara Petrovic <petrovic.ra21.2019@uns.ac.rs>
2022-11-13 15:11:08 +0100

    Added login form on frontend

--- BloodBankFrontend/src/app/components/login/login.component.ts
+++ BloodBankFrontend/src/app/components/login/login.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit{
 
-  constructor() { }
+  title = 'Login to Blood Bank';
+  username: string = "";
+  password: string = "";
+  submitted = false;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+
+  ) { }
 
   ngOnInit(): void {
+
+  }
+
+  onSubmit() {
+    this.submitted = true;
+ 
   }
 
 }
