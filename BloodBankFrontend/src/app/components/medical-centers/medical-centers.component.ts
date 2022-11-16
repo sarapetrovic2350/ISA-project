0fa92540601c2890cfb94da205c52dc8c3e5f195
Sara Petrovic <petrovic.ra21.2019@uns.ac.rs>
2022-11-17 00:44:03 +0100

    Implemented sort medical centers

--- BloodBankFrontend/src/app/components/medical-centers/medical-centers.component.ts
+++ BloodBankFrontend/src/app/components/medical-centers/medical-centers.component.ts
@@ -10,6 +10,7 @@ import { MedicalCenterServiceService } from 'src/app/service/medical-center.serv
 export class MedicalCentersComponent implements OnInit {
 
   medicalCenters: medicalCenter[] =[];
+  selectedOption: string = "";
   constructor(private medicalCenterService: MedicalCenterServiceService) { }
 
   ngOnInit(): void {
@@ -20,4 +21,25 @@ export class MedicalCentersComponent implements OnInit {
     })
   }
 
+  changeSorting() {
+    if(this.selectedOption == '1'){ 
+
+      return this.medicalCenters.sort((a, b) => a.name.localeCompare(b.name));
+
+    }else if(this.selectedOption == '3'){   
+
+      this.medicalCenters.sort(function(a, b) {
+
+        return b.averageGrade - a.averageGrade  })
+        
+    }else if(this.selectedOption == '2'){
+
+    return this.medicalCenters.sort((a, b) => a.address.city.localeCompare(b.address.city));
+ 
+    }
+    else {
+      return null;
+    }
+
+}
 }
