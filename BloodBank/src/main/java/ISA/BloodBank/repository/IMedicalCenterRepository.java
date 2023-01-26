ead799bd83a19792706d94e80bf20fbbfbb5957a
podrozujem <laragutman27@gmail.com>
2023-01-26 13:25:20 +0100

    Implemented conflict situations

--- BloodBank/src/main/java/ISA/BloodBank/repository/IMedicalCenterRepository.java
+++ BloodBank/src/main/java/ISA/BloodBank/repository/IMedicalCenterRepository.java
@@ -2,11 +2,19 @@ package ISA.BloodBank.repository;
 
 import java.util.List;
 
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import ISA.BloodBank.model.Appointment;
 import ISA.BloodBank.model.MedicalCenter;
 
 @Repository
@@ -22,4 +30,9 @@ public interface IMedicalCenterRepository extends JpaRepository<MedicalCenter, L
 
 //	@Query("SELECT mc FROM MedicalCenter mc, Address a WHERE mc.addressId=a.addressId and mc.name=?1 and a.city=?2")
 	List<MedicalCenter>findMedicalCentersByNameAndAddressCity(String name, String city);
+	
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select a from MedicalCenter a where a.id = :id")
+    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value ="0")})
+    public MedicalCenter findOneById(@Param("id")Long id);
 }
